3c383dad173dc48d35aaa17317ed783601f507d2
Jordan <jordan.m.edginton@gmail.com>
2022-05-08 17:43:40 -0600

    product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,47 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll(
+    {
+    include: [{
+      model: Category
+    },
+    {
+      model:Tag,
+      through:ProductTag
+    }
+
+  ]
+  })
+  .then((productData) => {
+    res.json(productData);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne(
+    {
+      where: {
+        id: req.params.id
+      },
+    include: [{
+      model: Category
+    },
+    {
+      model:Tag,
+      through:ProductTag
+    }
+
+  ]
+  })
+  .then((productData) => {
+    res.json(productData);
+  });
+  
+  
 });
 
 // create new product
@@ -91,6 +124,16 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
